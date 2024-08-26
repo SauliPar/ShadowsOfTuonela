a5917f017634aea7af743c9de71a105e47ef2f21
SauPar <sauli@empires.games>
2024-08-26 14:05:01 +0300

    added a singleton class

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance 
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = (T)FindObjectOfType(typeof(T));
+                if (instance == null)
+                {
+                    Debug.LogError("An instance of " + typeof(T) + " is needed in the scene, but there is none.");
+                }
+            }
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
