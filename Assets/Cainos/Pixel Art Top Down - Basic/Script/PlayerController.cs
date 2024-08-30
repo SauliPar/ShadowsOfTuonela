2dab6a36a0d31f1a07e130721d7bb47646b4a362
SauPar <sauli@empires.games>
2024-08-30 10:19:07 +0300

    brought back playercontroller because it turns out i was stupid when removing it.
    Added playercontroller into PlayerCharacter.prefab and its references

---
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/PlayerController.cs
@@ -0,0 +1,47 @@
+using Unity.Cinemachine;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : NetworkBehaviour
+{
+    [SerializeField] private NavMeshAgent Agent;
+    
+    private Vector3 _movePosition;
+
+    public void Start()
+    {
+        var cinemachineCamera = FindFirstObjectByType<CinemachineCamera>();
+        cinemachineCamera.LookAt = transform;
+        cinemachineCamera.Follow = transform;
+    }
+    
+    private void Update()
+    {
+        // if (HasStateAuthority == false)
+        // {
+        //     return;
+        // }
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Create a ray from the mouse cursor on screen in the direction of the camera
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+            // Check if the ray hits anything in the game world
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Print the position in world space
+                Debug.Log("World position: " + hit.point);
+                _movePosition = hit.point;        
+                MoveCharacter();
+            }
+        }
+    }
+    
+    private void MoveCharacter()
+    {
+        Agent.SetDestination(_movePosition);
+    }
+}
