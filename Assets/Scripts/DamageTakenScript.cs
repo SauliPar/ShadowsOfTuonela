b4796f8973a9cc611c90d69b733bfb0b70e9f59d
SauPar <sauli@empires.games>
2024-09-10 11:58:13 +0300

    added damagetaken into player prefab that works locally. Added a layer and changed the sorting layer from player to not be in front of everything

---
+++ Assets/Scripts/DamageTakenScript.cs
@@ -0,0 +1,38 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class DamageTakenScript : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private TextMeshProUGUI textMeshProUGUI;
+    private void Start()
+    {
+        HideDamageTakenUI();
+        EventManager.StartListening(Events.DamageEvent, ShowDamage);
+    }
+
+    private void ShowDamage(object data)
+    {
+        if (data == null) return;
+        
+        CancelInvoke();
+       
+        var damageTaken = (int)data;
+        
+        textMeshProUGUI.text = damageTaken.ToString();
+        canvasGroup.alpha = 1f;
+        
+        Invoke(nameof(HideDamageTakenUI), 1f);
+    }
+
+    private void HideDamageTakenUI()
+    {
+        canvasGroup.alpha = 0f;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.StopListening(Events.DamageEvent, ShowDamage);
+    }
+}
